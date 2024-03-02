a4bec235ccbf62bea46d5d0bc9482d41ecf67178
Shweta-7322 <shwetawalunj2002@gmail.com>
2024-03-02 11:54:53 +0530

    ATM Interfece

--- ATMInterface.java
+++ ATMInterface.java
@@ -1,5 +1,8 @@
 import java.util.Scanner;
-public class ATMInterface {   
+public class ATMInterface { 
+    private static final int MAX_TRANSACTIONS=100;
+    private static String[] transactionHistory = new String[MAX_TRANSACTIONS];
+    private static int transactionCount=0;  
     public static void main(String args[] )  
     {    
         int balance = 100000, withdraw, deposit;    
@@ -7,17 +10,26 @@ public class ATMInterface {
           
         while(true)  
         {  
-            System.out.println("Automated Teller Machine");  
-            System.out.println("Choose 1 for Withdraw");  
-            System.out.println("Choose 2 for Deposit");  
-            System.out.println("Choose 3 for Check Balance");  
-            System.out.println("Choose 4 for EXIT");  
+            System.out.println("Automated Teller Machine"); 
+            System.out.println("Choose 1 to Check Transaction History");
+            System.out.println("Choose 2 for Withdraw");  
+            System.out.println("Choose 3 for Deposit");  
+            System.out.println("Choose 4 for Check Balance");  
+            System.out.println("Choose 5 for Quit");  
             System.out.print("Choose the operation you want to perform:");  
               
             int choice = sc.nextInt();  
         switch(choice)  
         {  
-        case 1:  
+        case 1:
+        System.out.println("Transaction History:");
+        for(int i=0; i<transactionCount;i++)
+        {
+            System.out.println("transactionHistory[i]");
+        }
+        break;
+    
+        case 2:  
         System.out.print("Enter money to be withdrawn:");  
         withdraw = sc.nextInt();    
         if(balance >= withdraw)  
@@ -31,20 +43,20 @@ public class ATMInterface {
         }  
         System.out.println("");  
         break;  
-        case 2:  
+        case 3:  
         System.out.print("Enter money to be deposited:");   
         deposit = sc.nextInt();   
         balance = balance + deposit;  
-        System.out.println("Your Money has been successfully depsited");  
-        System.out.println("");  
-        break;  
-   
-        case 3:  
-        System.out.println("Balance : "+balance);  
+        System.out.println("Your Money has been successfully depo4sited");  
         System.out.println("");  
         break;  
    
         case 4:  
+        System.out.println("Balance : "+balance);  
+        System.out.println("");  
+        break;  
+   
+        case 5:  
         System.exit(0);  
       }  
     }  
